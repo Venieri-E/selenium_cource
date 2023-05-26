8b370996bb45e543882d9febdf190774679eb44a
Eugen Venieri <106976929+Venieri-E@users.noreply.github.com>
2023-05-26 18:26:51 +0300

    testing site 999.md

--- test_999_md.py
+++ test_999_md.py
@@ -19,7 +19,6 @@ def browser():
 
 # Random input values
 
-
 def generate_random_username(length=8):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for _ in range(length))
